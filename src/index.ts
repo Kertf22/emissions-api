78cdbf2f58f4c4ad3c9453e22939639f473564dc
Paulo Hercílio <paulo.hercilio2015@gmail.com>
2023-06-28 16:34:13 -0300

    primeiro endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,144 +6,55 @@ const app = express()
 
 app.use(express.json())
 
-app.post(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body
+//Quais são os x países com as maiores emissões de CO2 totais no x registrado?
 
-  const postData = posts?.map((post: Prisma.PostCreateInput) => {
-    return { title: post?.title, content: post?.content }
-  })
+app.get(`/co2-total/:paises_count`, async (req, res) => {
 
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: postData,
-      },
-    },
-  })
-  res.json(result)
-})
+  const { paises_count } = req.params;
 
-app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-  res.json(result)
-})
-
-app.put('/post/:id/views', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const post = await prisma.post.update({
-      where: { id: Number(id) },
-      data: {
-        viewCount: {
-          increment: 1,
-        },
-      },
-    })
-
-    res.json(post)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const postData = await prisma.post.findUnique({
-      where: { id: Number(id) },
-      select: {
-        published: true,
-      },
-    })
-
-    const updatedPost = await prisma.post.update({
-      where: { id: Number(id) || undefined },
-      data: { published: !postData?.published },
-    })
-    res.json(updatedPost)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.delete({
+  const data = await prisma.co2.groupBy({
+    by: ['country'],
     where: {
-      id: Number(id),
-    },
-  })
-  res.json(post)
-})
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
-
-app.get('/user/:id/drafts', async (req, res) => {
-  const { id } = req.params
-
-  const drafts = await prisma.user
-    .findUnique({
-      where: {
-        id: Number(id),
-      },
-    })
-    .posts({
-      where: { published: false },
-    })
-
-  res.json(drafts)
-})
-
-app.get(`/post/:id`, async (req, res) => {
-  const { id }: { id?: string } = req.params
-
-  const post = await prisma.post.findUnique({
-    where: { id: Number(id) },
-  })
-  res.json(post)
-})
-
-app.get('/feed', async (req, res) => {
-  const { searchString, skip, take, orderBy } = req.query
-
-  const or: Prisma.PostWhereInput = searchString
-    ? {
-        OR: [
-          { title: { contains: searchString as string } },
-          { content: { contains: searchString as string } },
-        ],
+      NOT: {
+        country: "Global"
       }
-    : {}
-
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-      ...or,
     },
-    include: { author: true },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
+    _sum: {
+      total: true,
+    },
+    take:Number(paises_count),
     orderBy: {
-      updatedAt: orderBy as Prisma.SortOrder,
-    },
-  })
+      _sum: {
+        total: "desc"
+      }
+    }
+  });
+  const format = data.map(({ _sum: { total }, country }) => ({
+    country, total
+  }))
+  res.status(200)
+  res.send(format);
+  res.end()
+});
 
-  res.json(posts)
-})
+app.get(`/co2-total/:country`, async (req, res) => {
+  const data = await prisma.co2.groupBy({
+    by: ['country'],
+    _sum: {
+      total: true,
+    },
+    orderBy: {
+      _sum: {
+        total: "desc"
+      }
+    }
+  });
+
+  res.status(200)
+  res.send(data);
+  res.end()
+});
+
 
 const server = app.listen(3000, () =>
   console.log(`
